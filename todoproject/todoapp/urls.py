930bf3b6b1657f1f981e2e167c9b598b8f58a532
Sreeraj <ssreeraj581@gmail.com>
2023-10-09 23:00:00 +0530

    added

---
+++ todoproject/todoapp/urls.py
@@ -0,0 +1,23 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+# app_name='todoapp'
+
+urlpatterns = [
+    path('',views.add,name='add'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+    path('cbvdetail/<int:pk>/',views.TaskDetailview.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.TaskUpdateView.as_view(),name='update'),
+    path('cbvdelete/<int:pk>/',views.TaskDeleteView.as_view(),name='delete')
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL,
+#                           document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
---
+++ todoproject/todoproject/todoapp/urls.py
@@ -0,0 +1,18 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+# app_name='todoapp'
+
+urlpatterns = [
+    path('',views.add,name='add'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update')
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL,
+#                           document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
